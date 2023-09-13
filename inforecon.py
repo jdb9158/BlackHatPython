914ebe87ddf3c16c549432c785ed108f4751d485
Justin Balroop <60368544+jdb9158@users.noreply.github.com>
2023-09-12 21:31:15 -0400

    Add files via upload

---
+++ inforecon.py
@@ -0,0 +1,24 @@
+import sys
+import requests
+import socket
+import json
+
+if len(sys.argv) < 2:
+    print("Usage: " + sys.argv[0] + "<url>")
+    sys.exit(1)
+
+req = requests.get("https://" + sys.argv[1])
+print("\n" + str(req.headers))
+
+gethostby = socket.gethostbyname(sys.argv[1])
+print("\nThe IP address of " + sys.argv[1] + " is: " + gethostby + "\n")
+
+#ipinfo.io
+
+req2 = requests.get("https://ipinfo.io/" + gethostby + "/json")
+response = json.loads(req2.text)
+
+print("Location: " + response["loc"])
+print("Region: " + response["region"])
+print("City: " + response["city"])
+print("Country: " + response["country"])
